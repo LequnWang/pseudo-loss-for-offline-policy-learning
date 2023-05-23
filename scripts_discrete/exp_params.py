9d9f42ceb2e3ddd78428e37aa72c60fee9058091
Lequn Wang <lw633@cornell.edu>
2023-05-23 12:59:11 -0400

    add prepare data

---
+++ scripts_discrete/exp_params.py
@@ -0,0 +1,31 @@
+exp_token = "d_eps0.1"
+exp_dir ="./exp_real_eps0.1"
+split_size = 5000
+submit = True
+data_ids = [247, 261, 1183, 1214]
+data_sizes = [0.01, 0.1, 1.]
+max_data_size_for_EB = 2.
+data_id_to_name = {
+    247: "BNGLetter",
+    261: "BNGPenDigits",
+    1183: "BNGSatImage",
+    1214: "BNGJPVowel"
+}
+data_names = [data_id_to_name[data_id] for data_id in data_ids]
+data_home = "./data"
+train_logging_policy_data_size = 1000
+epsilon = 0.1
+test_proportion = 0.3
+reward_model_proportion = 0.1
+estimators = ["IPW", "DR"]
+oracles = ["policy", "regression"]
+TEST_BAD_POLICY = False
+GENERATE_REAL_COST = True
+TIMES_NUM_LABELS = 1
+# SOFT_MAX_POLICY = False
+BINARY_COST = True
+betas = [0., 1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1.]
+lrs = [1e-3, 1e-2, 1e-1, 1., 10.]
+lrs = [str(lr) for lr in lrs]
+weight_decays = [1e-6]
+n_runs = 50
---
+++ scripts_discrete/prepare_data.py
@@ -0,0 +1,94 @@
+from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.utils import shuffle
+import pickle
+import os
+import sys
+import numpy as np
+sys.path.append(".")
+from params_exp_data_size import *
+from src.oracles import LinearRegressionCSOracle
+
+
+
+def generate_random_cost(Y):
+    num_classes_original = np.max(Y) + 1
+    num_classes = num_classes_original * TIMES_NUM_LABELS
+    cost_matrix = np.random.rand(num_classes_original, num_classes)
+    for i in range(num_classes_original):
+        for j in range(TIMES_NUM_LABELS):
+            cost_matrix[i, num_classes_original * j + i] = 0.
+    C = []
+    for y in Y:
+        C.append(cost_matrix[y, :])
+    C = np.array(C)
+    return C
+
+def generate_cost(Y):
+    num_classes_original = np.max(Y) + 1
+    num_classes = num_classes_original * TIMES_NUM_LABELS
+    cost_matrix = np.ones((num_classes_original, num_classes))
+    for i in range(num_classes_original):
+        for j in range(TIMES_NUM_LABELS):
+            cost_matrix[i, num_classes_original * j + i] = 0.
+    C = []
+    for y in Y:
+        C.append(cost_matrix[y, :])
+    C = np.array(C)
+    return C
+
+
+def fetch_data(data_id, data_home, train_logging_policy_data_path, test_data_path, simulate_bandit_feedback_data_path,
+               train_logging_policy_data_size, test_proportion):
+    dataset = fetch_openml(data_id=data_id, data_home=data_home, cache=True, as_frame=False)
+    X = dataset.data
+    Y = dataset.target
+    test_size = int((Y.shape[0] - train_logging_policy_data_size) * test_proportion)
+
+    encoder = LabelEncoder()
+    Y = encoder.fit_transform(Y)
+    if GENERATE_RANDOM_COST:
+        C = generate_random_cost(Y)
+    else:
+        C = generate_cost(Y)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    X, C = shuffle(X, C)
+    print(X.shape)
+    # print("valid data size {}".format(int((X.shape[0] - train_logging_policy_data_size) * (1 - test_proportion) / 2)))
+    print(C.shape)
+    with open(train_logging_policy_data_path, 'wb') as f:
+        pickle.dump((X[: train_logging_policy_data_size], C[: train_logging_policy_data_size]), f)
+    with open(simulate_bandit_feedback_data_path, 'wb') as f:
+        pickle.dump((X[train_logging_policy_data_size: -test_size], C[train_logging_policy_data_size: -test_size]), f)
+    with open(test_data_path, 'wb') as f:
+        pickle.dump((X[-test_size:], C[-test_size:]), f)
+    return X[: train_logging_policy_data_size], C[: train_logging_policy_data_size], X[-test_size:], C[-test_size:]
+
+
+if __name__ == "__main__":
+    for data_id in data_ids:
+        print(data_id)
+        data_name = data_id_to_name[data_id]
+        print(data_name)
+        train_logging_policy_data_path = os.path.join(exp_dir, data_name + ".train_logging_policy.pkl")
+        test_data_path = os.path.join(exp_dir, data_name + ".test.pkl")
+        simulate_bandit_feedback_data_path = os.path.join(exp_dir, data_name + ".simulate_bandit_feedback.pkl")
+        X_log, C_log, X_test, C_test = fetch_data(data_id, exp_dir, train_logging_policy_data_path, test_data_path,
+                                  simulate_bandit_feedback_data_path, train_logging_policy_data_size, test_proportion)
+        logging_policy = LinearRegressionCSOracle()
+        if TEST_BAD_POLICY:
+            logging_policy.fit(X_log, 1. - C_log)
+        else:
+            logging_policy.fit(X_log, C_log)
+        logging_policy_path = os.path.join(exp_dir, data_name + ".logging_policy.pkl")
+        with open(logging_policy_path, 'wb') as f:
+            pickle.dump(logging_policy, f)
+
+        # evaluate logging policy performance
+        Y_hat_test = logging_policy.predict(X_test)
+        Y_hat_test = np.eye(C_test.shape[1])[Y_hat_test]
+        average_cost = np.sum(Y_hat_test * C_test) / C_test.shape[0]
+        print(data_name, "average cost of logging policy: {}".format(average_cost))
+
